aaaeb763dfc053f48de1e06d7ade5719567388d1
Ario Tamaddoni <ariozone6@gmail.com>
2018-09-13 19:37:57 -0700

    Added a keydown event listener for Arrow keys on the document that faces the car in the direction pressed.#2

--- main.js
+++ main.js
@@ -7,6 +7,23 @@ class Car {
   }
   turn(direction) {
     this.direction = direction
+    switch (this.direction) {
+      case 'north':
+        this.$img.classList.add('north')
+        this.$img.classList.remove('east', 'west', 'south')
+        break
+      case 'east':
+        this.$img.classList.add('east')
+        this.$img.classList.remove('west', 'south', 'north')
+        break
+      case 'south':
+        this.$img.classList.add('south')
+        this.$img.classList.remove('east', 'west', 'north')
+        break
+      case 'west':
+        this.$img.classList.add('west')
+        this.$img.classList.remove('east', 'north', 'south')
+    }
   }
 }
 var carImage = document.createElement('img')
@@ -14,6 +31,20 @@ carImage.setAttribute('src', 'http://cdn.framesynthesis.com/html5/drivingsimulat
 document.body.appendChild(carImage)
 
 var car = new Car(carImage, 10, 'south', [0, 0])
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowUp') {
+    car.turn('north')
+  }
+  if (e.key === 'ArrowRight') {
+    car.turn('east')
+  }
+  if (e.key === 'ArrowDown') {
+    car.turn('south')
+  }
+  if (e.key === 'ArrowLeft') {
+    car.turn('west')
+  }
+})
 
 // carImage.classList.add('west')
 // carImage.classList.add('south')
